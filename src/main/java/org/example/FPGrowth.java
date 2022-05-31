eb73fa17b3db29524c979f68420493b7e516aa7a
zhenwei <cao.zhenwei@foxmail.com>
2022-05-31 20:40:00 +0800

    add code to construct fp-tree

---
+++ src/main/java/org/example/FPGrowth.java
@@ -0,0 +1,190 @@
+package org.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.apache.hadoop.io.IOUtils;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+public class FPGrowth {
+    public FPGrowth() {
+    }
+
+    public ArrayList<ArrayList<Integer>> trans;  // a list of transaction
+    public ArrayList<Integer> sortedItems;
+    public Map<Integer, Integer> map;  // key: itemId, value: count
+
+    public Map<Integer, Node> headTable;
+    public Node root;
+    public int maxLevel;
+
+
+     public int originTransNum = 0;
+//    public double minSupp = 0.092;
+
+    public static void main(String[] args) {
+        FPGrowth fpGrowth = new FPGrowth();
+        try {
+            String host = "hdfs://127.0.0.1:9000/";
+            //String inputFileUri = "/user/oslab/data/TestCase/test_2W.txt";
+            String inputFileUri = "/user/test1";
+            double minSupp = 0.2;
+            fpGrowth.readFile(host, inputFileUri);
+            fpGrowth.getHeadTable(minSupp);
+            fpGrowth.getFPTree();
+        } catch (Exception e) {
+            System.out.println("catch an exception" + e);
+            return;
+        } finally {
+            System.out.println("this is finally");
+        }
+
+    }
+
+    class Node{
+        public Node(Integer val, Integer id){
+            value = val;
+            itemId = id;
+            parentNode = null;
+            childNodes = new ArrayList<Node>();
+            itemNode = null;
+        }
+        public Node getChildNode(Integer id) {
+            for (Node childNode : childNodes) {
+                if (childNode.itemId == id) {
+                    return childNode;
+                }
+            }
+            return null;
+        }
+        public int value;
+        public int itemId;
+        public Node parentNode;  // pointer to its parent for node in fp-tree
+        public List<Node> childNodes ;  // pointer to its child
+        public Node itemNode;  // pointer to next same item in fp-tree
+    }
+public void readFile(String host, String inputFileUri) throws Exception{
+        // get origin map and transaction
+        Configuration config = new Configuration();
+        config.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+        FileSystem fs = FileSystem.get(URI.create(host), config);
+        FSDataInputStream is = fs.open(new Path(inputFileUri));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+        map = new HashMap<Integer, Integer>();
+        // transactions file, every line represent a record
+        String lineText = null;
+        trans = new ArrayList<>();
+        while ((lineText = bufferedReader.readLine()) != null) {
+            originTransNum += 1;
+            String[] items = lineText.split(" ");
+            ArrayList<Integer> transaction = new ArrayList<>();
+            for (String item : items) {
+                Integer itemId = Integer.parseInt(item);
+                transaction.add(itemId);
+                if (map.containsKey(itemId)) {
+                    map.put(itemId, map.get(itemId) + 1);
+                } else {
+                    map.put(itemId, 1);
+                }
+            }
+            trans.add(transaction);
+        }
+    }
+
+    public void getHeadTable(double minSupp){
+        long minNum = Math.round(originTransNum * minSupp);
+//        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        Iterator<ArrayList<Integer>> transIterator = trans.iterator();
+        maxLevel = 0;
+        while(transIterator.hasNext()){
+            // delete item does not satisfy min support threshold
+            ArrayList<Integer> transaction = transIterator.next();
+            Iterator<Integer> iterator2 = transaction.iterator();
+            while(iterator2.hasNext()){
+                Integer itemId= iterator2.next();
+                if(!map.containsKey(itemId)){
+                    iterator2.remove();
+                    continue;
+                }
+                if(map.get(itemId) < minNum){
+                    iterator2.remove();
+                    map.remove(itemId);
+                }
+            }
+            if (transaction.size() > maxLevel){
+                maxLevel = transaction.size();
+            }
+            if(transaction.isEmpty()){
+                transIterator.remove();
+            }
+        }
+        for(ArrayList<Integer> transaction: trans){
+            // sort every transaction by occurrence desc
+            transaction.sort((o1, o2)-> map.get(o2) - map.get(o1));
+        }
+
+        sortedItems = new ArrayList<>(map.keySet());
+        sortedItems .sort((o1, o2) -> map.get(o2) - map.get(o1));  // order by occurrence times desc
+//        list.forEach(System.out::println);
+        headTable = new HashMap<>(map.size());
+        for(Integer key: map.keySet()){
+            headTable.put(key, new Node(-1, -1));
+        }
+        System.out.println("end");
+    }
+
+    public void getFPTree(){
+        root = new Node(-1, -1);
+        for(ArrayList<Integer> transaction: trans){
+            Node parent = root;
+            int i = 0;
+            boolean newTree = false;
+            while(i < transaction.size()){
+                Integer itemId = transaction.get(i);
+                Node tmpNode = parent.getChildNode(itemId);
+                if(!newTree && tmpNode != null){  // node i already exist in level i
+                    tmpNode.value += 1;
+                    parent = tmpNode;
+                    i++;
+                    continue;
+                } else {
+                    newTree = true;
+                }
+                Node newNode = new Node(1, itemId);
+                parent.childNodes.add(newNode);
+                newNode.parentNode = parent;
+                if (headTable.get(itemId).parentNode == null){
+                    headTable.get(itemId).parentNode = newNode;
+                    headTable.get(itemId).itemNode = newNode;
+                } else{
+                    headTable.get(itemId).parentNode.itemNode = newNode;
+                    headTable.get(itemId).parentNode = newNode;
+                }
+                parent = newNode;
+                i++;
+            }
+        }
+        printTree(root);
+        System.out.println("end of tree");
+    }
+
+    public void printTree(Node root){
+        if(root.childNodes.isEmpty()){
+            System.out.print(" " + root.value + " ");
+        }else {
+            for (Node pointer : root.childNodes) {
+                printTree(pointer);
+            }
+            System.out.print(" " + root.value + " ");
+        }
+    }
+
+}
+
+
